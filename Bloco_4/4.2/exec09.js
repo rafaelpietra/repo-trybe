934840fc68e85737bbe21e9c31640f31aec901fb
Rafael Pietra <rafaelpietra@gmail.com>
2020-10-27 10:33:30 -0300

    Exercise number 09

---
+++ Bloco_4/4.2/exec09.js
@@ -0,0 +1,6 @@
+let array08 = [1,  2,  3,  4,  5,  6,  7,  8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25]
+let array08DivBy02 = [];
+ for (let i = 0; i < array08.length;i += 1){
+    array08DivBy02.push(array08[i]/2);
+ }
+ console.log(array08DivBy02);
