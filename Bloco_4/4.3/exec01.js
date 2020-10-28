d923d9c85adfcb8b65f4e3d8d86f4cd40879994d
Rafael Pietra <rafaelpietra@gmail.com>
2020-10-28 10:46:09 -0300

    Exercise number03

--- Bloco_4/4.3/exec01.js
+++ Bloco_4/4.3/exec01.js
@@ -3,7 +3,7 @@ let outPut = ''
 let symbol = '*'
 for (let addSymbol = 1; addSymbol <= nAsterisco; addSymbol +=1) {
      outPut += symbol;
-};
+}; 
 
 for (let linha = 1; linha <= 5; linha +=1) {
     console.log(outPut);
